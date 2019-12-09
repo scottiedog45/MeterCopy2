0a4a6ae4837283795ceff39a6733d8044e43c43f
Scott O <scottiedog45@gmail.com>
2019-12-09 13:27:46 -0500

    adding travis and gradle

---
+++ src/test/java/FooTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/*
+ * Java
+ *
+ * Copyright 2019 IS2T. All rights reserved.
+ * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ *
+ */
+class FooTest {
+
+	@Test
+	void test() {
+		Assertions.assertTrue(true);
+	}
+
+}
